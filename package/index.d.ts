6030b23e744209b4c5e9bed9510f9610da9c1a0e
pohadkove <80869110+pohadkove@users.noreply.github.com>
2022-12-09 17:36:03 -0800

    buuild

--- package/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as Alert } from "./Alert.svelte";
-export { default as Avatar } from "./Avatar.svelte";
-export { default as Button } from "./Button.svelte";
-export { default as Card } from "./Card.svelte";
-export { default as Fab } from "./Fab.svelte";
